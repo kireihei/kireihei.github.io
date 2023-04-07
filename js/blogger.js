5c707a4f41632663e9314af97a820d6738aea8fb
kireihei <47026893+kireihei@users.noreply.github.com>
2023-04-07 11:02:41 +0800

    Update blogger.js

--- js/blogger.js
+++ js/blogger.js
@@ -1,27 +1,35 @@
   const apiKey = "AIzaSyCv7WBhGTDfNhRfPmh4DttQtvdKSOhb-rU";
   const blogId = "6443783516671868516";
 
-  async function fetchLatestPost() {
-    const url = `https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}&maxResults=1`;
-    const response = await fetch(url);
-    const data = await response.json();
+async function fetchLatestPost() {
+  const url = `https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}&maxResults=1`;
+  const response = await fetch(url);
+  const data = await response.json();
 
-    if (data.items && data.items.length > 0) {
-      const post = data.items[0];
-      const postElement = document.createElement("div");
+  if (data.items && data.items.length > 0) {
+    const post = data.items[0];
+    const postElement = document.createElement("div");
 
-      const title = document.createElement("h3");
-      title.innerHTML = post.title;
-      postElement.appendChild(title);
+    const title = document.createElement("h2");
+    title.innerHTML = post.title;
+    postElement.appendChild(title);
 
-      const content = document.createElement("div");
-      content.innerHTML = post.content;
-      postElement.appendChild(content);
-
-      document.getElementById("latest-post").appendChild(postElement);
+    // Limit the content to 400 words
+    const content = document.createElement("div");
+    const contentText = post.content.replace(/<\/?[^>]+(>|$)/g, "");
+    const contentWords = contentText.split(" ");
+    if (contentWords.length > 400) {
+      contentWords.length = 400;
+      content.innerHTML = contentWords.join(" ") + " ...";
     } else {
-      console.error("No posts found");
+      content.innerHTML = post.content;
     }
-  }
+    postElement.appendChild(content);
 
-  fetchLatestPost();
+    document.getElementById("latest-post").appendChild(postElement);
+  } else {
+    console.error("No posts found");
+  }
+}
+
+fetchLatestPost();
